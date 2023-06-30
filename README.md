ba912e36bdfc86de3e3092cef97a5f24826e2a86
Niranjan-KS <131178323+niranjan-ks-pro@users.noreply.github.com>
2023-06-30 19:01:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# YesTechBottomNavigationBarPart29
+ https://www.youtube.com/watch?v=22XW4oN6Z1o&list=PL1JrLEBAapUWv_JSolgE5pdcDKWw80wKA&index=29
---
+++ android/app/src/main/kotlin/com/example/yestechbottomnavigationbarpart29/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yestechbottomnavigationbarpart29
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
